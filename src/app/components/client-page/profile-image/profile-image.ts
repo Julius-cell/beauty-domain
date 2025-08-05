a40e3642013039f8d817302ded4b64103d22684e
Julio Cid beroiza <julio.cid.b@gmail.com>
2025-08-05 01:34:08 -0400

    feat: add icon image change

--- src/app/components/client-page/profile-image/profile-image.ts
+++ src/app/components/client-page/profile-image/profile-image.ts
@@ -1,8 +1,11 @@
 import { Component, input, output } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'cp-profile-image',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './profile-image.html',
   styleUrl: './profile-image.css'
 })
@@ -11,6 +14,7 @@ export class ProfileImage {
   imageChange = output<File>();
 
   defaultAvatar = 'https://ui-avatars.com/api/?name=Cliente&background=random';
+  faPencilAlt = faPencilAlt;
 
   onFileSelected(event: Event) {
     const file = (event.target as HTMLInputElement)?.files?.[0];
