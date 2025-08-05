497d204ebfc73f17309f0beb54fc15a3c7fc0d38
Julio Cid beroiza <julio.cid.b@gmail.com>
2025-08-05 01:28:07 -0400

    chore: add tailwindcss

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClientPage } from './components/client-page/client-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ClientPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
